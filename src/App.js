bac24c23c6cd6509ab7f7c0a1a7fcfdc75a86cc3
pedrobernardo94 <pedro.bernardo@mindera.com>
2023-03-23 18:12:30 +0000

    feat(no-US): final version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import { checkForLocalStorageWallets, createAccount } from "./utils";
+import "./App.css";
+import WalletItem from "./components/wallet-item";
+
+let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+  const [walletPassword, setWalletPassword] = useState("");
+
+  useEffect(() => {
+    checkForLocalStorageWallets(setAccounts);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Simple Wallet Keeper Binance</p>
+      <span>Password: &nbsp;</span>
+      <input
+        type="password"
+        aria-label="wallet-password"
+        placeholder="Password"
+        onChange={(e) => setWalletPassword(e.target.value)}
+        onKeyDown={(e) =>
+          e.key === "Enter" && createAccount(web3, walletPassword, setAccounts)
+        }
+        value={walletPassword.current}
+      />
+      <button onClick={() => createAccount(web3, walletPassword, setAccounts)}>
+        Create Wallet
+      </button>
+      <div>
+        {accounts.map((acc) => (
+          <WalletItem key={acc.address} account={acc} web3={web3} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,105 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+afterEach(() => localStorage.clear());
+
+test("renders App title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Simple Wallet Keeper Binance/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("renders password label", () => {
+  render(<App />);
+  const inputLabelElement = screen.getByText(/Password:/i);
+  expect(inputLabelElement).toBeInTheDocument();
+});
+
+test("renders create wallet button", () => {
+  render(<App />);
+  const createButtonElement = screen.getByText(/Create Wallet/i);
+  expect(createButtonElement).toBeInTheDocument();
+});
+
+test("should create a wallet without a password and show to the user", async () => {
+  render(<App />);
+  const createButtonElement = screen.getByText(/Create Wallet/i);
+
+  fireEvent.click(createButtonElement);
+
+  const refreshButton = await screen.findByText("Refresh");
+  expect(refreshButton).toBeInTheDocument();
+});
+
+test("should create a wallet with a click and a password and show to the user", async () => {
+  render(<App />);
+  const createButtonElement = screen.getByText(/Create Wallet/i);
+  const walletPasswordInput = screen.getByLabelText("wallet-password");
+
+  fireEvent.change(walletPasswordInput, { target: { value: "password" } });
+
+  fireEvent.click(createButtonElement);
+
+  const decryptButton = await screen.findByText("Decrypt Private Key");
+  expect(decryptButton).toBeInTheDocument();
+});
+
+test("should create a wallet without a password with the enter key and show to the user", async () => {
+  render(<App />);
+  const walletPasswordInput = screen.getByLabelText("wallet-password");
+
+  fireEvent.focus(walletPasswordInput);
+  fireEvent.keyDown(walletPasswordInput, {
+    key: "Enter",
+    code: "Enter",
+    charCode: 13,
+  });
+
+  const refreshButton = await screen.findByText("Refresh");
+  expect(refreshButton).toBeInTheDocument();
+});
+
+test("should create a wallet and decrypt the private key with a password", async () => {
+  render(<App />);
+  const createButtonElement = screen.getByText(/Create Wallet/i);
+  const walletPasswordInput = screen.getByLabelText("wallet-password");
+
+  fireEvent.change(walletPasswordInput, { target: { value: "password" } });
+
+  fireEvent.click(createButtonElement);
+
+  const decryptButton = await screen.findByText("Decrypt Private Key");
+  const walletItemPasswordInput = screen.getByLabelText(
+    "wallet-item-password-input"
+  );
+
+  fireEvent.change(walletItemPasswordInput, { target: { value: "password" } });
+  fireEvent.click(decryptButton);
+
+  const privateKeyText = await screen.findByText("Private Key:", {
+    exact: false,
+  });
+  expect(privateKeyText).toBeInTheDocument();
+});
+
+test("should fail to decrypt the private key with an invalid password", async () => {
+  jest.spyOn(console, "log").mockImplementation(() => {});
+  render(<App />);
+  const createButtonElement = screen.getByText(/Create Wallet/i);
+  const walletPasswordInput = screen.getByLabelText("wallet-password");
+
+  fireEvent.change(walletPasswordInput, { target: { value: "password" } });
+
+  fireEvent.click(createButtonElement);
+
+  const decryptButton = await screen.findByText("Decrypt Private Key");
+  const walletItemPasswordInput = screen.getByLabelText(
+    "wallet-item-password-input"
+  );
+
+  fireEvent.change(walletItemPasswordInput, { target: { value: "wrong" } });
+  fireEvent.click(decryptButton);
+
+  const invalidPasswordText = await screen.findByText("Invalid Password");
+  expect(invalidPasswordText).toBeInTheDocument();
 });
